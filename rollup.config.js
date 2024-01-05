952119e6ce582a814d2a33acb033b1190eb06700
ShetePro <sheteprolin@gmail.com>
2024-01-05 16:42:22 +0800

    build(环境配置): rollup 和 ts 配置

---
+++ rollup.config.js
@@ -0,0 +1,66 @@
+import { defineConfig } from "rollup";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import image from "@rollup/plugin-image";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
+import postcss from "rollup-plugin-postcss";
+import autoprefixer from "autoprefixer";
+import postcssImport from "postcss-import";
+import cssnano from "cssnano";
+import path from "path";
+
+const __dirname = "/";
+export default defineConfig({
+  input: "src/index.js",
+  output: [
+    {
+      file: "dist/bundle.js",
+      format: "es",
+      sourcemap: false,
+    },
+    {
+      file: "dist/index.umd.js",
+      format: "umd",
+      name: "index",
+    },
+    {
+      file: "dist/BlockMusicPlayer.min.js",
+      format: "iife",
+      name: "BlockMusicPlayer",
+    },
+  ],
+  plugins: [
+    commonjs(),
+    typescript(),
+    json(),
+    image(),
+    postcss({
+      // 内联css
+      minimize: true,
+      extensions: [".css", ".scss"],
+      to: path.resolve(__dirname, "dist/assets/*"),
+      plugins: [
+        autoprefixer(),
+        postcssImport(),
+        cssnano({
+          preset: "default", // 使用默认配置
+        }),
+      ],
+    }),
+    babel({
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            targets: "last 2 versions, > 0.5%, IE 11",
+          },
+        ],
+      ],
+      plugins: ["@babel/plugin-transform-optional-chaining"], // 启用可选链语法支持
+      include: ['js', 'ts', 'vue'],
+      exclude: "node_modules/**",
+      babelHelpers: "inline",
+    }),
+  ],
+});
