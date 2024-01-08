170e1169094b93e666a425dee5abda0f9ab4923d
ShetePro <sheteprolin@gmail.com>
2024-01-08 14:42:25 +0800

    feat(control 层): 添加控制层按钮

--- src/player/index.ts
+++ src/player/index.ts
@@ -1,3 +1,5 @@
+import playIcon from '../assets/play_icon.svg'
+import stopIcon from '../assets/stop_icon.svg'
 interface BlockMusicPlayerOption {
   container: HTMLElement | null
   audioList?: AudioConfig[]
@@ -15,10 +17,12 @@ class BlockMusicPlayer {
   playerIndex = 0;
   audioList;
   audio = document.createElement('audio');
+  control = {
+    play: false
+  }
   constructor(option: BlockMusicPlayerOption) {
     this.container = option.container
     this.audioList = option.audioList
-    console.log(this.container)
     this.initPlayer()
 
   }
@@ -26,12 +30,26 @@ class BlockMusicPlayer {
     if (!this.container) return
     this.renderCover()
     this.renderAudio()
+    this.renderControl()
     const playerBody = document.createElement('div')
     playerBody.classList.add('block-music-player', 'is-mini')
     playerBody.append(this.fragment)
     this.container.append(playerBody)
   }
 
+  renderControl () {
+    const controlBox = document.createElement('div')
+    controlBox.classList.add('music-control')
+    const playControl = document.createElement('img')
+    playControl.src = this.control.play ? playIcon : stopIcon
+    playControl.classList.add('music-control-play')
+    playControl.addEventListener('click', () => {
+      this.control.play = !this.control.play
+      playControl.src = this.control.play ? playIcon : stopIcon
+    })
+    controlBox.append(playControl)
+    this.fragment.append(controlBox)
+  }
   renderAudio () {
     const audio = this.audio || document.createElement('audio')
     audio.src = '../assets/xq.mp3'
