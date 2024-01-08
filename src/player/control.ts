bed1756b2518f864586dc0bdbb8947f9b00345d3
ShetePro <sheteprolin@gmail.com>
2024-01-08 17:19:27 +0800

    feat(control 层): 导入mp3 文件控制播放暂停

---
+++ src/player/control.ts
@@ -0,0 +1,26 @@
+export type ControlOption = {
+  play: boolean
+  index: number
+  words: boolean
+  audio: null | HTMLAudioElement
+}
+export default class MusicControl {
+  state: ControlOption;
+  constructor(option: ControlOption) {
+    this.state = option;
+  }
+  toggle () {
+    this.state.play ? this.stop() : this.play()
+    console.log(this.state.audio)
+  }
+  play() {
+    this.state.play = true
+    this.state.audio?.play()
+  }
+  stop() {
+    this.state.play = false
+    this.state.audio?.pause()
+  }
+
+  next() {}
+}
--- src/player/index.ts
+++ src/player/index.ts
@@ -1,70 +1,75 @@
-import playIcon from '../assets/play_icon.svg'
-import stopIcon from '../assets/stop_icon.svg'
+import playIcon from "../assets/play_icon.svg";
+import stopIcon from "@src/assets/stop_icon.svg";
+import MusicControl from "./control";
 interface BlockMusicPlayerOption {
-  container: HTMLElement | null
-  audioList?: AudioConfig[]
+  container: HTMLElement | null;
+  audioList?: AudioConfig[];
 }
 
 type AudioConfig = {
-  cover: string
-  title: string
-  artist: string
-  url: string
-}
+  cover: string;
+  title: string;
+  artist: string;
+  url: string;
+};
 class BlockMusicPlayer {
-  container;
+  container: HTMLElement | null;
   fragment = document.createDocumentFragment();
   playerIndex = 0;
-  audioList;
-  audio = document.createElement('audio');
-  control = {
-    play: false
-  }
+  audioList: AudioConfig[] | undefined;
+  audio = document.createElement("audio");
+  control: MusicControl;
   constructor(option: BlockMusicPlayerOption) {
-    this.container = option.container
-    this.audioList = option.audioList
-    this.initPlayer()
-
+    this.container = option.container;
+    this.audioList = option.audioList;
+    this.control = new MusicControl({
+      play: false,
+      index: 0,
+      words: false,
+      audio: this.audio,
+    });
+    this.initPlayer();
   }
-  initPlayer () {
-    if (!this.container) return
-    this.renderCover()
-    this.renderAudio()
-    this.renderControl()
-    const playerBody = document.createElement('div')
-    playerBody.classList.add('block-music-player', 'is-mini')
-    playerBody.append(this.fragment)
-    this.container.append(playerBody)
-  }
-
-  renderControl () {
-    const controlBox = document.createElement('div')
-    controlBox.classList.add('music-control')
-    const playControl = document.createElement('img')
-    playControl.src = this.control.play ? playIcon : stopIcon
-    playControl.classList.add('music-control-play')
-    playControl.addEventListener('click', () => {
-      this.control.play = !this.control.play
-      playControl.src = this.control.play ? playIcon : stopIcon
-    })
-    controlBox.append(playControl)
-    this.fragment.append(controlBox)
-  }
-  renderAudio () {
-    const audio = this.audio || document.createElement('audio')
-    audio.src = '../assets/xq.mp3'
-    this.fragment.append(audio)
+  initPlayer() {
+    if (!this.container) return;
+    this.renderCover();
+    this.renderAudio();
+    this.renderControl();
+    const playerBody = document.createElement("div");
+    playerBody.classList.add("block-music-player", "is-mini");
+    playerBody.append(this.fragment);
+    this.container.append(playerBody);
   }
 
-  renderCover () {
-    const cover = document.createElement('div')
-    cover.classList.add('music-cover')
-    const coverImg = document.createElement('img')
-    cover.append(coverImg)
-    coverImg.src = 'https://w.wallhaven.cc/full/jx/wallhaven-jxl31y.png'
-    coverImg.classList.add('music-cover-img')
-    this.fragment.append(cover)
+  renderControl() {
+    const controlBox = document.createElement("div");
+    controlBox.classList.add("music-control");
+    const playControl = document.createElement("img");
+    playControl.src = this.control.state.play ? stopIcon : playIcon;
+    playControl.classList.add("music-control-play");
+    playControl.addEventListener("click", () => {
+      this.control?.toggle();
+      playControl.src = this.control.state.play ? stopIcon : playIcon;
+    });
+    controlBox.append(playControl);
+    this.fragment.append(controlBox);
+  }
+  renderAudio() {
+    const audio = this.audio || document.createElement("audio");
+    audio.src = "../src/assets/xq.mp3";
+    this.fragment.append(audio);
+    this.audio = audio;
+  }
+
+  renderCover() {
+    const cover = document.createElement("div");
+    cover.classList.add("music-cover");
+    const coverImg = document.createElement("img");
+    cover.append(coverImg);
+    coverImg.src = "https://w.wallhaven.cc/full/jx/wallhaven-jxl31y.png";
+    coverImg.classList.add("music-cover-img");
+    this.fragment.append(cover);
   }
 }
 
-export default BlockMusicPlayer
+export default BlockMusicPlayer;
