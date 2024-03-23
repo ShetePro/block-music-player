0485dc402e1f04b95be5bf227086ecccb871a59b
ShetePro <sheteprolin@gmail.com>
2024-03-23 23:56:27 +0800

    feat(进度条): 设置进度条跟随鼠标事件 进度条悬浮放大

--- src/js/player/processBar.ts
+++ src/js/player/processBar.ts
@@ -24,15 +24,30 @@ export class ProcessBar {
       append: [this.setBar(), this.setMusicTime()],
     });
 
-    this.dom.append(box)
+    this.dom.append(box);
     return this.dom;
   }
-  setBar () {
-    const line = createDocumentEl('div', {classList: ['bar-line']})
-    const pointer = createDocumentEl('span', {classList: ['bar-pointer']})
-    line.append(pointer)
-    return line
+  setBar() {
+    const line = createDocumentEl("div", { classList: ["bar-line"] });
+    const fullLine = createDocumentEl("div", { classList: ["full-line"] });
+    const wrap = createDocumentEl("div", { classList: ["wrap"], append: [fullLine] });
+    const pointer = createDocumentEl("span", { classList: ["bar-pointer"] });
+    line.append(wrap, pointer);
+    const lineMoveEvent = (e: MouseEvent) => {
+      if (e.offsetX !== undefined && e.target === wrap) {
+        fullLine.setAttribute('style',  `transform:translateX(${e.offsetX}px);`)
+      }
+    };
+    wrap.addEventListener("mouseenter", () => {
+      wrap.addEventListener("mousemove", lineMoveEvent);
+    });
+    wrap.addEventListener("mouseleave", () => {
+      wrap.removeEventListener("mouseover", lineMoveEvent);
+      fullLine.setAttribute('style',  `transform:translateX(${0}px);`)
+    });
+    return line;
   }
+
   setMusicTime() {
     const time = createDocumentEl("div", {
       classList: ["music-time"],
