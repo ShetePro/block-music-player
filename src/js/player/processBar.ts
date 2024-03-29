c37ca04f0e5598a9ce4ca8baec44450015006137
ShetePro <sheteprolin@gmail.com>
2024-03-29 23:58:29 +0800

    feat(进度条): 进度条点击控制和悬浮显示进度时间

--- src/js/player/processBar.ts
+++ src/js/player/processBar.ts
@@ -10,42 +10,69 @@ export class ProcessBar {
   audio: HTMLAudioElement;
   dom: DocumentFragment;
   control: MusicControl;
+  pointer: HTMLSpanElement | undefined;
+  wrap: HTMLDivElement | undefined;
   timer: number;
+  progress: number;
 
   constructor(opt: ProcessBarOption) {
     this.audio = opt.audio;
     this.dom = document.createDocumentFragment();
     this.control = opt.control;
     this.timer = 0;
+    this.progress = 0;
   }
   createContainer() {
     const box = createDocumentEl("div", {
       classList: ["process-bar"],
       append: [this.setBar(), this.setMusicTime()],
     });
-
     this.dom.append(box);
     return this.dom;
   }
   setBar() {
     const line = createDocumentEl("div", { classList: ["bar-line"] });
     const fullLine = createDocumentEl("div", { classList: ["full-line"] });
-    const wrap = createDocumentEl("div", { classList: ["wrap"], append: [fullLine] });
+    const wrap = createDocumentEl("div", {
+      classList: ["wrap"],
+      append: [fullLine],
+    });
     const pointer = createDocumentEl("span", { classList: ["bar-pointer"] });
-    line.append(wrap, pointer);
+    const popover = createDocumentEl("div", { classList: ["bar-popover"] });
+    line.append(wrap, pointer, popover);
+    this.wrap = wrap;
+    this.pointer = pointer;
     const lineMoveEvent = (e: MouseEvent) => {
       if (e.offsetX !== undefined && e.target === wrap) {
-        fullLine.setAttribute('style',  `transform:translateX(${e.offsetX}px);`)
+        fullLine.setAttribute("style", `transform:translateX(${e.offsetX}px);`);
+        popover.setAttribute("style", `transform:translateX(${e.offsetX}px);`);
+        const rate = e.offsetX / wrap.offsetWidth;
+        popover.innerText = this.calcTimeByRate(rate);
       }
     };
     wrap.addEventListener("mouseenter", () => {
       wrap.addEventListener("mousemove", lineMoveEvent);
+      popover.classList.add("popover-show");
     });
     wrap.addEventListener("mouseleave", () => {
       wrap.removeEventListener("mouseover", lineMoveEvent);
-      fullLine.setAttribute('style',  `transform:translateX(${0}px);`)
+      fullLine.setAttribute("style", `transform:translateX(${0}px);`);
+      popover.classList.remove("popover-show");
+    });
+    wrap.addEventListener("click", (e) => {
+      const rate = e.offsetX / wrap.offsetWidth;
+      this.audio.currentTime = this.audio.duration * rate;
+      this.progressMove()
     });
     return line;
+  }
+
+  progressMove() {
+    const current = this.audio.currentTime;
+    const progressTime = this.audio.duration - current
+    this.progress = 1 - (progressTime / this.audio.duration);
+    const x = this.wrap ? this.progress * this.wrap.offsetWidth : 0;
+    this.pointer?.setAttribute("style", `transform:translateX(${x}px);`);
   }
 
   setMusicTime() {
@@ -56,13 +83,21 @@ export class ProcessBar {
     if (this.timer) return time;
     this.timer = setInterval(() => {
       const current = this.audio.currentTime;
-      time.innerText = transformSecond(this.audio.duration - current);
+      const progressTime = this.audio.duration - current
+      this.progressMove();
+      time.innerText = transformSecond(progressTime);
       if (current === this.audio.duration) {
         console.log("播放完了 下一首");
+        this.pointer?.setAttribute("style", `transform:translateX(0px);`);
         this.control.nextAudio();
         this.audio.play();
       }
     }, 1000);
     return time;
+  }
+
+  calcTimeByRate(rate: number) {
+    const time = this.audio.duration * rate;
+    return transformSecond(time);
   }
 }
